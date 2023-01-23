b04afb7864a3915bf0614ffd6a06b0ac33e2b0cf
Jean Egalon <egalon.jean@gmail.com>
2023-01-23 15:11:00 -0300

    feito a tela de registro

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -1,7 +1,51 @@
 import React from 'react';
 
 export default function Register() {
+  const onSubmit = (e) => {
+    console.log('oi');
+    e.preventDefault();
+  };
+
   return (
-    <div>Register</div>
+    <form onSubmit={ onSubmit }>
+      <label htmlFor="newUserName">
+        Nome
+        <br />
+        <input
+          id="newUserName"
+          type="text"
+          placeholder="Seu Nome"
+          data-testid="common_register__input-name"
+        />
+      </label>
+      <br />
+      <label htmlFor="newUserEmail">
+        Email
+        <br />
+        <input
+          id="newUserEmail"
+          type="email"
+          placeholder="seu-email@site.com.br"
+          data-testid="common_register__input-email"
+        />
+      </label>
+      <br />
+      <label htmlFor="newUserPassword">
+        Senha
+        <br />
+        <input
+          id="newUserPassword"
+          type="password"
+          placeholder="**********"
+          data-testid="common_register__input-password"
+        />
+      </label>
+      <br />
+      <input
+        type="submit"
+        value="CADASTRAR"
+        data-testid="common_register__input-register"
+      />
+    </form>
   );
 }
