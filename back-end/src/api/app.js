716572fd7e5c2291a3fd85d7974c95bcc232ae2c
David Junior <david.lourenco94@gmail.com>
2023-01-24 18:58:24 -0300

    add statics image

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -10,6 +10,7 @@ const customerRoute = require('../Routes/customerRoute');
 
 const app = express();
 app.use(express.json());
+app.use(express.static('public'));
 
 app.use('/login', loginRouter);
 app.use('/register', registerRoute);
